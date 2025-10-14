492cedab6328d41c4674af3e52a0904ad0eadb99
Andrei Olaru <olaruandreidan@gmail.com>
2025-10-14 13:47:37 +0300

    Added redundancy timer to Time Limit Popup

--- TimeLimitPopup.xaml.cs
+++ TimeLimitPopup.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace KioskApp
 {
@@ -11,6 +12,8 @@ namespace KioskApp
     {
         private static readonly ILogger logger = LogManager.GetLogger();
         private Window parentWindow;
+        private DispatcherTimer countdownTimer;
+        private int remainingSeconds = 20;
 
         public TimeLimitPopup(string gameName, string imagePath, Window parent)
         {
@@ -23,6 +26,9 @@ namespace KioskApp
 
             // Load image if path is provided
             LoadImage(imagePath);
+
+            // Start countdown timer
+            StartCountdownTimer();
         }
 
         private void LoadImage(string imagePath)
@@ -64,7 +70,59 @@ namespace KioskApp
         private void DismissButton_Click(object sender, RoutedEventArgs e)
         {
             logger.Info("Time limit popup dismissed by user.");
+            StopCountdownTimer();
             parentWindow?.Close();
+        }
+
+        private void StartCountdownTimer()
+        {
+            try
+            {
+                countdownTimer = new DispatcherTimer();
+                countdownTimer.Interval = TimeSpan.FromSeconds(1);
+                countdownTimer.Tick += CountdownTimer_Tick;
+
+                // Update initial countdown text
+                UpdateCountdownText();
+
+                countdownTimer.Start();
+                logger.Info("Started 20-second auto-close countdown for popup");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Failed to start countdown timer");
+            }
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            remainingSeconds--;
+
+            if (remainingSeconds <= 0)
+            {
+                logger.Info("Auto-closing popup after 20-second countdown");
+                StopCountdownTimer();
+                parentWindow?.Close();
+            }
+            else
+            {
+                UpdateCountdownText();
+            }
+        }
+
+        private void UpdateCountdownText()
+        {
+            CountdownText.Text = $"This window will close automatically in {remainingSeconds} seconds";
+        }
+
+        private void StopCountdownTimer()
+        {
+            if (countdownTimer != null)
+            {
+                countdownTimer.Stop();
+                countdownTimer.Tick -= CountdownTimer_Tick;
+                countdownTimer = null;
+            }
         }
     }
 }
